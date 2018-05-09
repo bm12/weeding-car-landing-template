1685ed5449f034fec32fb28444735460c2bd25f9
bm12 <mahmud1997babaev@mail.ru>
2018-05-09 20:25:44 +0300

    fix scroll, add more info, fix map, delete <g> from svg

--- js/script.js
+++ js/script.js
@@ -1,68 +1,113 @@
-$(document).ready(function() {
-	/* noUiSlider start*/
-	var slider = document.getElementById('price_slider');
-	noUiSlider.create(slider, {
-		start: [0, 80000],
-		connect: true,
-		range: {
-			'min': 600,
-			'max': 80000
-		}
-	});
-
-	var valuesDivs = [
-		document.getElementById('price_car_from'),
-		document.getElementById('price_car_to')
-	];
-	slider.noUiSlider.on('update', function( values, handle ) {
-		valuesDivs[handle].innerHTML = Math.round(values[handle]) + ' Р.';
-	});
-	/* noUiSlider end */
-	$('.car_sld').owlCarousel({
-		loop:true,
-		margin:0,
-		nav:true,
-		items:1,
-		navText: ['<img src="img/arrow_left.png" alt="">','<img src="img/arrow_left.png" alt="">']
-	});
-    
-    $('.mob_car_class').click(function() {
-       $('.mob_car_class_wrap').slideToggle(400);
-    });
-    var myMap;
-    ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
-    function init () {
-        myMap = new ymaps.Map("map", {
-          center: [55.76, 37.64], // Координаты центра карты
-          zoom: 10 // Zoom
-        });
-    }
-    $('a[href^="#"]').click(function () { 
-         var elementClick = $(this).attr("href");
-         var destination = $(elementClick).offset().top;
-           $('body').animate( { scrollTop: destination }, 1100 );
-         return false;
-    });
-    $('#mob_menu').click(function() {
-        $(".mob_nav").addClass('active');
-    });
-    $('.mob_nav').click(function(e) {
-        e.stopPropagation();
-        $('.mob_nav').removeClass('active');
-    });
-    $('.mob_nav_ul').click(function(e) {
-       e.stopPropagation(); 
-    });
-    $('.mob_nav_ul a').click(function() {
-        $('.mob_nav').removeClass('active');
-    });
-    $('.callAutoBtn').click(function() {
-        $('#ordAuto').fadeIn(400);
-    });
-    $('#ordAuto').click(function() {
-        $('#ordAuto').fadeOut(400);
-    });
-    $('.modal').click(function(e) {
-        e.stopPropagation();
-    });
+$(document).ready(function () {
+    /* noUiSlider start*/
+    var slider = document.getElementById('price_slider');
+    noUiSlider.create(slider, {
+        start: [0, 80000],
+        connect: true,
+        range: {
+            'min': 600,
+            'max': 80000
+        }
+    });
+
+
+    var valuesDivs = [
+        document.getElementById('price_car_from'),
+        document.getElementById('price_car_to')
+    ];
+
+    slider.noUiSlider.on('update', function (values, handle) {
+        valuesDivs[handle].innerHTML = Math.round(values[handle]) + ' Р.';
+    });
+
+    /* noUiSlider end */
+
+    $('.car_sld').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        navText: ['<img src="img/arrow_left.png" alt="">', '<img src="img/arrow_left.png" alt="">']
+    });
+
+
+    $('.mob_car_class').click(function () {
+        $('.mob_car_class_wrap').slideToggle(400);
+    });
+
+    $('a[href^="#"]').click(function (e) {
+        e.preventDefault();
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        $('html, body').animate({ scrollTop: destination }, 1100);
+    });
+
+    $('#mob_menu').click(function () {
+        $(".mob_nav").addClass('active');
+    });
+
+    $('.mob_nav').click(function (e) {
+        e.stopPropagation();
+        $('.mob_nav').removeClass('active');
+    });
+
+    $('.mob_nav_ul').click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('.mob_nav_ul a').click(function () {
+        $('.mob_nav').removeClass('active');
+    });
+
+    $('.callAutoBtn, .car_order').click(function () {
+        $('#ordAuto').fadeIn(400);
+    });
+
+    $('#ordAuto').click(function () {
+        $('#ordAuto').fadeOut(400);
+    });
+
+    $('.modal').click(function (e) {
+        e.stopPropagation();
+    });
+
+    $('.wrap_all_car').on('click', '.car_more', function(e) {
+        e.stopPropagation();
+        var $that = $(this);
+        var template = $('#more_info_template').clone().html();
+        $that.parent().parent().after(template);
+        $('.more_small_owl').owlCarousel({
+            loop: true,
+            margin: 15,
+            nav: true,
+            dots: false,
+            responsive : {
+                0 : {
+                    margin: 5,
+                    items: 2
+                },
+                768 : {
+                    margin: 10,
+                    items: 3
+                },
+                1190 :{
+                    margin: 15,
+                    items: 4
+                }
+            },
+            navText: ['<img src="./img/back.svg" alt="">','<img src="./img/back.svg" alt="">']
+        });
+        $('.wrap_more_info').slideDown(400);
+    });
+
+    $('.wrap_all_car').on('click', '.wrap_more_info', function(e) {
+        e.stopPropagation();
+    });
+    
+    $('body').click(function() {
+        $('.wrap_more_info').slideUp(350, function(){
+            $('.wrap_more_info').remove();
+        });
+    });
+
 });
